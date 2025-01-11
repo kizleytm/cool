39e8dc37ff3b2e7f19719480f47f3a009574230e
kizleytm <kizleytmdc@yahoo.com>
2025-01-11 15:43:59 +0000

    Add files via upload

---
+++ mapshit.bat
@@ -0,0 +1,21 @@
+@echo off
+echo mappin rq
+if not exist mapper.exe (
+echo an antivirus prob delete or you are inside the rar
+pause >nul
+exit
+)
+echo by conspiracy as a test.
+echo this is part of clavish (discord.gg/clavish)
+echo press any key to spoof! tested on 22h2 and 20H2!
+pause >nul
+color b
+echo spoofing...
+mapper.exe "diskhandler.sys"
+mapper.exe "SMBiosHandler.sys"
+echo spoofed!
+echo.
+echo.
+echo made by conspiracy
+echo discord.gg/clavish for more
+pause>nul
